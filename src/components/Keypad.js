d50b015b44006ad6208c54257617b7e9f1995d61
Agent-grey <markkibuthu@gmail.com>
2024-08-06 23:02:16 +0300

    complete lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,21 @@
 // Code Keypad Component Here
+import React from "react";
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  const handleChange = () => {
+    console.log('Entering password...');
+  };
+
+  return (
+    <div>
+      {/* Render an input field of type "password" */}
+      <input
+        type="password"
+        onChange={handleChange} 
+        placeholder="Enter password"
+      />
+    </div>
+  );
 }
 
 export default Keypad;
